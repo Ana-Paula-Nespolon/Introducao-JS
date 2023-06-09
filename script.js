14985b36ff84afcd09fddff5e9c7cd2bb2714995
ANA PAULA NESPOLON <127706266+Ana-Paula-Nespolon@users.noreply.github.com>
2023-06-09 17:44:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+<script>
+    var alerta = ('Meu nome é Ana Paula!');
+
+    
+    function imprimeAlerta(){
+        alert('Meu nome é Ana Paula');
+        imprimeAlerta('Meu nome é Ana Paula');
+    }
+
+    function abreAlerta(){
+        alert('Meu nome é Ana Paula');
+    }   
+
+</script> 
